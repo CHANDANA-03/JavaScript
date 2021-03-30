8611da596f07bd958ac71c0a6a1713a0b38c216d
CHANDANA-03 <79985013+CHANDANA-03@users.noreply.github.com>
2021-03-30 16:06:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+//const myform = document.querySelector('#my-form');
+//const nameInput = document.querySelector('#name');
+//const emailInput = document.querySelector('#email');
+//const msg = document.querySelector('.msg');
+//const userList = document.querySelector('#user');
+
+//myform.addEventListener('submit', onSubmit);
+//function onSubmit(e) {
+  // e.preventDefault();
+   //if(nameInput.value === '' || emailInput.value === '') {
+     // msg.classList.add('error');
+      //msg.innerHTML = 'please enter all fields';
+     // setTimeout(() => msg.remove(), 3000);
+   //}else {
+     // const li = document.createElement('li');
+     // li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+      
+   //userList.appendChild(li); 
+
+      //clear fields
+      //nameInput.value = '';
+      //emailInput.value = '';
+   //}
+//}
+
+let greet1 = "Hello World!";
+const PI = 3.14; 
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Exporting variables and functions
+export { greet1, PI, multiplyNumbers };
